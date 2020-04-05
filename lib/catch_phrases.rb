99c6fb7e95a7254a1d260e92ba877798bc744d52
Eric Johnson <johnson.n.eric@gmail.com>
2020-04-05 18:13:35 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,14 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+  puts status
 end
 
 def toadstool
   puts status
 end
+
+def link
+  phrase = "It's Dangerous To Go Alone! Take This."
+  phrase
+end
+
